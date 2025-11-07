41d46e0dffbbd7039e81dbd0ebf7eab21a972ab9
Lenchik16 <leonidkovalev317@gmail.com>
2025-11-07 10:59:58 +1000

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+from datetime import date
+def saturdays_between_two_dates(start, end):
+    if start > end:
+        start, end = end, start
+    start_days = start.toordinal()
+    end_days = end.toordinal()
+    return sum(date.fromordinal(d).weekday() == 5 for d in range(start_days, end_days + 1)) 
+
+date1 = date(2020, 7, 26)
+date2 = date(2020, 7, 2)
+
+print(saturdays_between_two_dates(date1, date2))
